fabe08fb4282a67e0eb4b1428fb470403a28e222
Chibey-max <ilorahdavid126@gmail.com>
2026-09-25 15:09:43 +0100

    Add core Stellar account helpers and node example

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,14 @@ export { createSponsoredAccount } from "./stellar/account.js";
 export { setupMultisig, setupSessionKey, type SetupMultisigOpts, type SetupSessionKeyOpts } from "./stellar/multisig.js";
 export { buildFeeBump } from "./stellar/transaction.js";
 export { KNOWN_ASSETS, getAsset, getNativeAsset } from "./stellar/assets.js";
+export {
+  changeTrust,
+  claimClaimableBalance,
+  mergeAccount,
+  type ClaimClaimableBalanceOpts,
+  type MergeAccountOpts,
+  type TrustlineOpts,
+} from "./stellar/account-helpers.js";
 export { KeyManager } from "./keys/manager.js";
 export { PasskeyManager, type PasskeyCredential, type PasskeyRegistrationOpts, type PasskeyAssertionOpts } from "./keys/passkey.js";
 export { Wallet } from "./wallet/wallet.js";
